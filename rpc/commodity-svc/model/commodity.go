a97352f502b813e323861664309174cfb97d718d
cjq <3472189068@qq.com>
2023-02-14 22:23:32 +0800

    gorm自定义数据类型

--- rpc/commodity-svc/model/commodity.go
+++ rpc/commodity-svc/model/commodity.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"gorm.io/gorm"
 	"time"
 )
@@ -16,6 +18,38 @@ type Base struct {
 
 // 商品表
 type Commodity struct {
+	Base
+	CategoryId int32 `gorm:"type:int;not null"`
+	Category   Category
+	BrandId    int32 `gorm:"type:int;not null"`
+	Brand      Brand
+
+	Name string `gorm:"type:varchar(100);not null"`
+	// 商品的编号,下单后商家会根据这个编号去仓库拿货
+	Sn string `gorm:"type:varchar(100);not null"`
+	// 商品的阅读点击数量 销量 收藏数量
+	ReadNum int32 `gorm:"type:int;default:0;not null"`
+	FavNum  int32 `gorm:"type:int;default:0;not null"`
+	SaleNum int32 `gorm:"type:int;default:0;not null"`
+
+	// 市场平时价格 本地价格
+	commonPri float32 `gorm:";not null"`
+	LocalPri  float32 `gorm:"type:int;default:0;not null"`
+	// 简介的
+	EasyDesc string `gorm:"type:varchar(100);not null"`
+
+	// 进入商品后的图片
+	Images GormList `gorm:"type:varchar(2000);not null"`
+	// 未进入商品,封面图
+	FrontImage GormList `gorm:"type:varchar(2000);not null"`
+	// 商品详细描述中的图
+	DescImages []string `gorm:"type:varchar(100);not null"`
+
+	// 是否出售即上架 免运费 新品 热卖
+	IsNew      bool `gorm:"default:false;not null"`
+	IsSale     bool `gorm:"default:false;not null"`
+	IsFreeShip bool `gorm:"default:false;not null"`
+	IsHot      bool `gorm:"default:false;not null"`
 }
 
 // 商品分类表   主流电商系统都是三级分类 家用电器->电视->高清电视,每一级分类都有自己的信息,如果每一集定义一张表也就是个结构体,那么后续改动可能就要修改数据库
@@ -72,3 +106,27 @@ type Banner struct {
 	Url     string `gorm:"type:varchar(300);not null"`
 	Index   int32  `gorm:"type:int;default:1;not null"`
 }
+
+// gorm自定义数据类型
+// 存储[],实际上存储的是字符串
+type GormList []string {
+
+}
+
+//自定义的数据类型必须实现 Scanner 和 Valuer 接口，以便让 GORM 知道如何将该类型接收、保存到数据库
+// 实现 driver.Valuer 接口，Value 返回 json value
+func (g GormList) Value() (driver.Value, error) {
+	// 编码成json字符串
+	return json.Marshal(g)
+}
+
+// 实现 sql.Scanner 接口，Scan 将 value 扫描至 Jsonb
+func (g *GormList) Scan(value interface{}) error {
+	// 从数据库中拿出来
+	return json.Unmarshal(value.([]byte), g)
+}
+
+
+
+
+
