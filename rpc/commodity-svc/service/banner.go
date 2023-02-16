39af97b66adfb6914f416983d7eb0a12b53fb99c
cjq <3472189068@qq.com>
2023-02-17 00:10:39 +0800

    commodity_rpc轮播图增删改接口

--- rpc/commodity-svc/service/banner.go
+++ rpc/commodity-svc/service/banner.go
@@ -1,27 +1,78 @@
 package service
 
 import (
+	"commodity-rpc/dao"
+	"commodity-rpc/model"
 	"commodity-rpc/pb"
 	"context"
 	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // 轮播图列表
 func (c *CommodityServer) BannerList(context.Context, *empty.Empty) (*pb.BannerListRes, error) {
+	bannerListRes := pb.BannerListRes{}
 
+	var banners []model.Banner
+	result := dao.DB.Find(&banners)
+	bannerListRes.Total = int32(result.RowsAffected)
+
+	var bannerRes []*pb.BannerRes
+	for _, banner := range banners {
+		bannerRes = append(bannerRes, &pb.BannerRes{
+			Id:    banner.ID,
+			Image: banner.Picture,
+			Index: banner.Index,
+			Url:   banner.Url,
+		})
+	}
+
+	bannerListRes.Data = bannerRes
+	return &bannerListRes, nil
 }
 
 // 创建轮播图
-func (c *CommodityServer) CreateBanner(context.Context, *pb.BannerReq) (*pb.BannerRes, error) {
+func (c *CommodityServer) CreateBanner(ctx context.Context, req *pb.BannerReq) (*pb.BannerRes, error) {
+	banner := model.Banner{}
 
+	banner.Picture = req.Image
+	banner.Index = req.Index
+	banner.Url = req.Url
+
+	dao.DB.Save(&banner)
+
+	return &pb.BannerRes{Id: banner.ID}, nil
 }
 
 // 删除轮播图
-func (c *CommodityServer) DeleteBanner(context.Context, *pb.BannerReq) (*empty.Empty, error) {
-
+func (c *CommodityServer) DeleteBanner(ctx context.Context, req *pb.BannerReq) (*empty.Empty, error) {
+	if result := dao.DB.Delete(&model.Banner{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "此轮播图不存在")
+	}
+	return &emptypb.Empty{}, nil
 }
 
 // 更新轮播图
-func (c *CommodityServer) UpdateBanner(context.Context, *pb.BannerReq) (*empty.Empty, error) {
+func (c *CommodityServer) UpdateBanner(ctx context.Context, req *pb.BannerReq) (*empty.Empty, error) {
+	var banner model.Banner
 
+	if result := dao.DB.First(&banner, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "轮播图不存在")
+	}
+
+	if req.Url != "" {
+		banner.Url = req.Url
+	}
+	if req.Image != "" {
+		banner.Picture = req.Image
+	}
+	if req.Index != 0 {
+		banner.Index = req.Index
+	}
+
+	dao.DB.Save(&banner)
+
+	return &emptypb.Empty{}, nil
 }
